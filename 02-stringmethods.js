27cbde323115dd5218dfbfcd30fd4560df5fe326
Dev <dev1672004@gmail.com>
2025-06-19 16:07:36 +0530

    js search and methods

---
+++ 02-stringmethods.js
@@ -0,0 +1,33 @@
+// let text1 = "hello";
+// let text2 = "world!";
+// let text3 = text1.concat(" ", text2);
+// console.log(text3);
+
+
+
+//trim
+// let text1 = "           hello world";
+// let text2 = text1.trim();
+// console.log(text2);
+
+
+
+//pad start
+// let text = "5";
+// text = text.padStart(4, "0");
+// console.log(text);  // Output: "0005"
+
+
+
+//repeat
+// let text = " helloworld ";
+// let result = text.repeat(6);
+// console.log(result);
+
+
+
+
+//replacing content
+let text = " wonderful sky ";
+let newtext = text.replace( "sky", "blue");
+console.log(newtext);
